580f67bfa5e8fa4294256a660c3abda00aa23221
Gilberto <savoini@dac246.dac.unicamp.br>
2019-05-03 17:04:46 -0300

    RocketBook

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+const App = () => (
+  <React.Fragment>
+    <h1>Hello World 2</h1>
+  </React.Fragment>
+);
+
+render(<App />, document.getElementById('app'));
